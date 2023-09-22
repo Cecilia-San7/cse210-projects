82cff6fc9cde7861ad74fa2c80b039567aadedab
Cecilia-San7 <cecibeba31@gmail.com>
2023-09-21 23:04:31 -0600

    Finished Prep 3  Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number? ");
+        // string MagicNumber = Console.ReadLine();
+        // int TheMagicNum = int.Parse(MagicNumber);
+
+        Random randomGenerator = new Random();
+        int TheMagicNum = randomGenerator.Next(1, 11);
+
+        
+
+        string lower = "lower";
+        string higher = "higher";
+    
+        while (true)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            int MyGuess = int.Parse(guess);
+
+            if (MyGuess == TheMagicNum)
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+            else if (MyGuess <= TheMagicNum)
+            {
+                Console.WriteLine(higher);
+            }
+            else if (MyGuess >= TheMagicNum)
+            {
+                Console.WriteLine(lower);
+                
+            }
+
+     }
     }
 }
